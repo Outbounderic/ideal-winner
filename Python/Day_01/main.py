de3fe14f36dbfaa2b07628ce39ead2e5a7525ddd
Eric V <ericvanmeter57@gmail.com>
2022-08-16 18:53:35 -0400

    Starting to work on python again.

---
+++ Python/Day_01/main.py
@@ -0,0 +1,14 @@
+# Input function gathers user input
+# Print function displays the argument
+# Variables store a piece a data as a type
+
+# c = input("What is your name? ")
+
+# print("Hello " + c)
+
+print("Welcome to band name generator.")
+
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is the name of a pet? ")
+
+print("Your band name is " + city + " " + pet)
