60fe12464a930c602891f89bc256cfd33af80c8f
Eric V <ericvanmeter57@gmail.com>
2022-08-16 21:27:21 -0400

    Refresh on control flow.

---
+++ Python/Day_03/main.py
@@ -0,0 +1,14 @@
+# Control flow
+# Comparison Operators
+# > < >= <= == !=
+# num = True
+# if num == True:
+#     print("True")
+# elif num == "Other":
+#     print("Other")
+# else:
+#     print("False")
+
+
+# Logical Statements
+# or and not
